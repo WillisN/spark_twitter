8b2db29e42210c5042257df1aeb75604f7d469d2
WillisN <54052439+WillisN@users.noreply.github.com>
2020-09-28 21:21:18 -0400

    add_spark_project

---
+++ twitter_secure.py
@@ -0,0 +1,14 @@
+import json
+import requests_oauthlib
+
+def secure():
+    path = '/Users/willis/Desktop/Projets Code/Python/SparkStreaming/secret/'
+    # Load Twitter API secrets from a json file
+    secrets = json.loads(open(path + 'secrets.json').read())
+    api_key = secrets['API_key']
+    api_secret_key = secrets['API_secret_key']
+    access_token = secrets['access_token']
+    access_token_secret = secrets['access_token_secret']
+    my_auth = requests_oauthlib.OAuth1(api_key, api_secret_key, access_token, access_token_secret)
+
+    return my_auth
